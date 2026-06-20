46e0ac2482d3dffb591e1907ef68759204750c1d
Colek45 <colek45@gmail.com>
2026-06-19 19:22:52 -0700

    Added HP calculations

--- data_retrieval.py
+++ data_retrieval.py
@@ -6,7 +6,7 @@ def load_hp(level,role,tier,traits):
     with open('tables/monster_statistics.csv', mode='r') as file:
         reader = csv.DictReader(file)
         for row in reader:
-            if row['Level'] == level or row['Level'] == -1 and level == 'Novice':
+            if row['Level'] == level or (row['Level'] == '-1' and level == 'Novice'):
                 hp = int(row['Average HP'])
                 break
     with open('tables/monster_role.csv', mode='r') as file:
@@ -23,8 +23,7 @@ def load_hp(level,role,tier,traits):
         hp /= 2
     hp_modifier = 0
     for trait in traits:
-        
         if 'HP' in trait.name:
             hp_modifier += trait.bonus
     hp *= (1 + hp_modifier/100)
-    return hp
+    return int(hp)
